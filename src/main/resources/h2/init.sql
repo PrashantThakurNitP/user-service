39a0676f2dd09fa473dd07d4ce21d91ed18cae09
prashant09.thakur <prashant09.thakur@gmail.com>
2021-11-16 21:29:03 +0530

    delete user issue resolved

--- src/main/resources/h2/init.sql
+++ src/main/resources/h2/init.sql
@@ -10,5 +10,13 @@ create table user_transaction(
 	user_id bigint,
 	amount int,
 	transaction_date timestamp,
-	foreign key (user_id) references users(id)
+	foreign key (user_id) references users(id) on delete cascade
 );
+
+insert into users
+	(name,balance)
+	values
+	('sam',1000),
+	('mike',1200),
+	('jake',800),
+	('marshal',2000);
